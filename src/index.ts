7eff657d9e6c845753fa096aaefcc31b9fbe0674
Tal Yaron <tal.yaron@gmail.com>
2022-07-15 11:45:13 +0300

    sass

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import m from "mithril";
 import Home from "./view/pages/Home";
 import Second from "./view/pages/Second";
 
+import './view/styles/app.scss'
+
 const mountNode = document.querySelector("#app");
 if (mountNode) {
   m.route(document.body, "/home", {
